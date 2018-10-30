3c69c16869334166861169584a7978ffbb130e71
hautouit <37023303+hautouit@users.noreply.github.com>
2018-10-31 01:23:14 +0700

    STEP 1
    DESIGN INTERFACE AND DATABASE

---
+++ WebBanSach/WebBanSach/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        QuanLyBanSachEntities db = new QuanLyBanSachEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult SachMoiPartial()
+        {
+            var lsSachMoi = db.Saches.Take(3).ToList();
+            return PartialView(lsSachMoi);
+        }
+    }
+}
